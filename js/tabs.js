f9d88fd038fe886a7022920fba4e0b716486c03b
IZhuhan <zhugan.irina@gmail.com>
2018-03-29 19:45:48 +0300

    added some changes

--- js/tabs.js
+++ js/tabs.js
@@ -2,12 +2,14 @@ $( '.nav-tabs' ).on( 'click', 'li', function( e ) {
     e.preventDefault();
 
     const tabControls = $( this ).closest( '.nav-tabs' ).find( 'li' );
-    const tabPanes = $( this ).closest( '.nav-tabs' ).next().find( '.tab-pane' );
+    const tabPaneActive = $( this ).closest( '.nav-tabs' ).next().find( '.tab-pane.active' );
     const id = $( this ).find( 'a' ).attr( 'href' );
 
     tabControls.removeClass( 'active' );
-    tabPanes.removeClass( 'active in' );
-
     $( this ).addClass( 'active' );
-    $( `.tab-pane${ id }` ).addClass( 'active' ).fadeIn().addClass( 'in' );
+
+    tabPaneActive.fadeOut( () => {
+        tabPaneActive.removeClass( 'active' );
+        $( id ).fadeIn( () => $( id ).addClass( 'active' ) );
+    });
 });
